ee124a4b9a5124f0e6b1eaa8326e02e489b9a66f
wessal <wessal.h.alharbi@outlook.com>
2025-10-19 09:52:43 +0300

    Initial Commit

---
+++ WLearningJourney/WLearningJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WLearningJourneyApp.swift
+//  WLearningJourney
+//
+//  Created by wessal hashim alharbi on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct WLearningJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WLearningJourneyTests/WLearningJourneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  WLearningJourneyTests.swift
+//  WLearningJourneyTests
+//
+//  Created by wessal hashim alharbi on 19/10/2025.
+//
+
+import Testing
+@testable import WLearningJourney
+
+struct WLearningJourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
